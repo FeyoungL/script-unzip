85cf2c2f67fe14b454cd0ba5ad211e5315008cd0
Feyoung Liu <feiyang0ray@163.com>
2021-01-15 17:59:47 +0800

    210115 kill fonlab all exe

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Update
@@ -12,19 +13,23 @@ namespace Update
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Waiting for few seconds...");
+            Process[] fontLab = Process.GetProcessesByName("FontLab");
+            foreach (Process item in fontLab)
+            {
+                item.Kill();
+            }
             if (args.Length != 2)
             {
                 StringBuilder argument = new StringBuilder();
-                argument.AppendLine("Args format error, args length must be 2.")
+                argument.AppendLine("ERROR: Args format error, args length must be 2.")
                     .Append("\tUsed \"Update.exe SrcUnzipFilePath DestUnzipFolder.\"");
                 Console.WriteLine(argument);
                 return;
             }
 
-            Console.WriteLine("Src unzip file: {0}\r\nDest unzip Folder: {1}", args[0], args[1]);
-
             new ZipHelper().UnZip(args[0], args[1]);
-
+            Console.WriteLine("Src unzip file: {0}\r\nDest unzip Folder: {1}", args[0], args[1]);
             Console.WriteLine("Update/Unzip Done");
 
             string fullPath = args[1] + "\\" + "FontLab.exe";
